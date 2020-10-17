cbc17d0441877f9123a237eab53445aa8987038b
ajit <ajit1.biswal@gmail.com>
2020-10-17 09:32:39 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void annoride() {
+		System.out.println("annoride");
+	}
+
+	@Test
+	public void ios() {
+		System.out.println("ios");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/ExtentReportSemo.java
@@ -0,0 +1,32 @@
+package qaclickacademy;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportSemo {
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void config() {
+		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("tester","ajit biswal");
+		
+	}
+
+	
+	@Test
+	public void initialDemo() {
+		extent.createTest("initialDemo");
+		System.out.println("this is test");
+		extent.flush();
+	}
+}
